f1638e00147ed1f76d54bbd94a90a554e5fe3a46
Alex Scanlan <ajscanlan27@gmail.com>
2015-07-23 15:41:28 +0100

    Added killstreaks and killstreak sounds

--- RobotsVsHumans/src/com/ajscanlan/robotsvshumans/GameApp.java
+++ RobotsVsHumans/src/com/ajscanlan/robotsvshumans/GameApp.java
@@ -6,19 +6,31 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.print.attribute.standard.Media;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+
+
 public class GameApp {
 
 	ArrayList<Robot> robots = new ArrayList<Robot>();
 	ArrayList<Human> humans = new ArrayList<Human>();
+
 	ArrayList<String> firstNames = new ArrayList<String>();
 	ArrayList<String> lastNames = new ArrayList<String>();
 	ArrayList<String> roboNames = new ArrayList<String>();
 
-	final int LIMIT = 1000;
-	final float DECAY = 0.05F;
-	private int totalHumanPower, totalRobotPower;
+	private final int LIMIT = 1000;
+	private final float DECAY = 0.05F;
+	private int totalHumanPower, totalRobotPower, winStreak;
+	private int sleep = 2500;
+	private String champion;
 
 	public static void main(String[] args) {
+
 		GameApp gl = new GameApp();
 		int choice = gl.displayMenu();
 		gl.populateLists();
@@ -37,34 +49,110 @@ public class GameApp {
 
 	private void battleRoyale() {
 
+		String tempChamp = "";
+		String prevChamp = "";
+		int tempWinStreak = 0;
+
+		//checking 
 		while(humans.size() > 0 && robots.size() > 0){
 
+			System.out.println(tempChamp);
+
+			if(!tempChamp.equals(prevChamp)){
+				tempWinStreak = 0;
+			}
+			
+			
 			System.out.println(robots.get(0) + " vs " + humans.get(0));
+			
 
 			if(humans.get(0).getPower() > robots.get(0).getPower()){
 				robots.remove(0);
 				humans.get(0).setPower(humans.get(0).getPower() - (humans.get(0).getPower() * DECAY));
 				System.out.println("HUMAN WINNER");
+				prevChamp = tempChamp;
+				tempChamp = humans.get(0).getName();
+				calculateWinstreak(++tempWinStreak, humans.get(0).getName());
+				System.out.println("Current Kill Streak: " + tempWinStreak);
 			} else if(humans.get(0).getPower() == robots.get(0).getPower()){
 				robots.remove(0);
 				humans.remove(0);
 				System.out.println("DRAW");
+				System.out.println("Current Kill Streak: " + tempWinStreak);
 			} else if(humans.get(0).getPower() < robots.get(0).getPower()) {
 				humans.remove(0);
 				robots.get(0).setPower(robots.get(0).getPower() - (robots.get(0).getPower() * DECAY));
 				System.out.println("ROBOT WINNER");
+				prevChamp = tempChamp;
+				tempChamp = robots.get(0).getModel();
+				calculateWinstreak(++tempWinStreak, robots.get(0).getModel());
+				System.out.println("Current Kill Streak: " + tempWinStreak);
 			}
+			
+			System.out.println("");
 		}
 
 		System.out.println("Humans: " + humans.size());
 		System.out.println("Robots: " + robots.size());
+		System.out.println("Champion: " + champion + "\nKill Streak: " + winStreak);
+		
+		
+	}
 
+	private void calculateWinstreak(int tempWinStreak, String name) {
+		if(tempWinStreak > winStreak){
+			winStreak = tempWinStreak;
+			champion = name;
+		}
+
+		switch(tempWinStreak){
+		case 2:
+			System.out.println("Double Kill");
+			playSound("doublekill.wav");
+			break;
+		case 3:
+			System.out.println("Multi Kill");
+			playSound("multikill.wav");
+			break;
+		case 4:
+			System.out.println("Ultra Kill");
+			playSound("ultrakill.wav");
+			break;
+		case 5:
+			System.out.println("R-R-R-R-RAMPAGE");
+			playSound("rampage.wav");
+			break;
+		case 10:
+			System.out.println("HOLY SHIT");
+			System.out.println("GODLIKE");
+			playSound("godlike.wav");
+		}
+	}
+
+	private void playSound(String string) {
+		try {
+		    File yourFile = new File(string);
+		    AudioInputStream stream;
+		    AudioFormat format;
+		    DataLine.Info info;
+		    Clip clip;
+
+		    stream = AudioSystem.getAudioInputStream(yourFile);
+		    format = stream.getFormat();
+		    info = new DataLine.Info(Clip.class, format);
+		    clip = (Clip) AudioSystem.getLine(info);
+		    clip.open(stream);
+		    clip.start();
+		    Thread.sleep(sleep);
+		}
+		catch (Exception e) {
+		    //whatevers
+		}
 	}
 
 	private int displayMenu() {
 		System.out.println("1) 1 versus 1");
 		System.out.println("2) Battle Royale");
-		//System.out.println("Literally anything else) Quit");
 
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
